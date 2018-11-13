71b1847c62c3e281cc0a191d6bdf74bb9b3d8321
CarlosCanizal <carlos@canizal.com>
2018-11-13 13:18:20 -0600

    Updates request to HTTPS

--- cielo24/cielo24/web_utils.py
+++ cielo24/cielo24/web_utils.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 from __future__ import unicode_literals
 
-from httplib import HTTPConnection
+from httplib import HTTPSConnection
 from urllib import urlencode
 from urlparse import urlparse
 import json
@@ -32,7 +32,7 @@ class WebUtils(object):
             query = {}
         if headers is None:
             headers = {}
-        http_connection = HTTPConnection(base_uri, timeout=timeout)
+        http_connection = HTTPSConnection(base_uri, timeout=timeout)
         query_string = ('?' + urlencode(query)) if len(query) else ''
         # Will raise an error on timeout
         http_connection.request(method, path + query_string, body=body, headers=headers)
